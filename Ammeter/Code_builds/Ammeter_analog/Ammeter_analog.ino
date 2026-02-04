844bf3402f0e881fe53b3bd61d6b234ba37e4e0a
mn19 <mn19@uw.edu>
2026-02-03 20:58:03 -0800

    Working proto combo Ammeter_analog to Ammeter_display

--- Ammeter/Code_builds/Ammeter_analog/Ammeter_analog.ino
+++ Ammeter/Code_builds/Ammeter_analog/Ammeter_analog.ino
@@ -10,27 +10,32 @@ This code will also set the analog output from 0-10V for the panel meter and 0-5
 This will be the lightest code to keep it as fast as possible since the PLC has a lower clock speed than the Ammeter_display and Ammeter_udp MCUs.
 ******************************************/
 
+#include <P1AM.h>
 
-const int maxRaw = 4095;
-const int displayMCUReady = 2;
+// UART pins:
+// 14 = TX
+// 13 = RX
+const int displayMCUReady = 13; // Using the unused RX pin seems to work
 void setup() {
-  Serial.begin(115200);      // USB debug
-  Serial1.begin(500000);     // UART0
-  analogReadResolution(12);  // 0–4095
-  pinMode(26, OUTPUT);  // for pot
-  digitalWrite(26, HIGH); // for pot
-  pinMode(displayMCUReady, INPUT);  // from diaplay MCU 
-  pinMode(27, INPUT);  // analog read pin
+  Serial.begin(115200);   // USB debug
+  pinMode(displayMCUReady, INPUT);  // from diaplay MCU
+  Serial1.begin(500000);  // UART0
+  P1.init();
 }
 
 
 void loop() {
   while (digitalRead(displayMCUReady) == HIGH) {
-    uint16_t adcValue = analogRead(27);
+    uint16_t adcValue = P1.readAnalog(1, 1); //slot 1 channel 2;
+    Serial.println(adcValue);
     Serial1.write(0xA5);             // sync
     Serial1.write(adcValue & 0xFF);  // LSB
     Serial1.write(adcValue >> 8);    // MSB
     Serial.println(adcValue);
-    delay(20);
+    delay(10);
+    // P1.writeAnalog(adcValue, 2, 1); // // Need to correct scaling to 0-10V
+    // P1.writeAnalog(adcValue / 2, 2, 2); // Need to correct scaling to 0-5V
   }
+  Serial.println("Waiting for Ready");
+  delay(500);
 }
