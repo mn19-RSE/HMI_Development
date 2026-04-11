940078772dd4ca18a187972450e5124e1e0243d0
unknown <mn19@uw.edu>
2026-04-11 15:14:44 -0700

    movign up a bit

--- Ammeter/Tests/RaspberryPi5_powered/display_background.py
+++ Ammeter/Tests/RaspberryPi5_powered/display_background.py
@@ -74,7 +74,7 @@ def draw_screen():
 
     # Big voltage display
     volt_text = font_large.render(f"{voltage:+.5f} {unit}", True, DYNAMIC_COLOR)
-    canvas.blit(volt_text, (500, 300))
+    canvas.blit(volt_text, (450, 250))
 
     # Bar graph
     draw_bar(voltage)
