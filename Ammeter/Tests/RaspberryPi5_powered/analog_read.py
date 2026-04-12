74dea65c9d78b4f0e3a800c43015546c31650fb6
unknown <mn19@uw.edu>
2026-04-11 17:09:20 -0700

    reduced analog read test

--- Ammeter/Tests/RaspberryPi5_powered/analog_read.py
+++ Ammeter/Tests/RaspberryPi5_powered/analog_read.py
@@ -1,9 +1,6 @@
-from daqhats import mcc118, HatIDs
-from daqhats_utils import select_hat_device
+from daqhats import mcc118
 
-# initialize once
-address = select_hat_device(HatIDs.MCC_118)
-hat = mcc118(address)
+hat = mcc118(0)
 
 def read_voltage():
     return hat.a_in_read(0)
