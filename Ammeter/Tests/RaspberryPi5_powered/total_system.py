fde0bb71f33d99e62ffe84eee97595169ca01f97
unknown <mn19@uw.edu>
2026-04-11 17:53:37 -0700

    removed the braces to make it work

--- Ammeter/Tests/RaspberryPi5_powered/total_system.py
+++ Ammeter/Tests/RaspberryPi5_powered/total_system.py
@@ -100,8 +100,7 @@ while running:
             running = False
 
 
-    voltage = read_voltage()
-    voltage = f"{voltage}" * f"{scale_voltage_multipliers[scale_value]}"
+    voltage = read_voltage() * scale_voltage_multipliers[scale_value]
     draw_screen(voltage)
     clock.tick(60)  # 60 FPS
 
