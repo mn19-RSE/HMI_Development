4960ab5b69b74d5aa06e25b69b44a71b0345a545
unknown <mn19@uw.edu>
2026-04-12 19:15:14 -0700

    starting to get correct OD cup location

--- Ammeter/Tests/RaspberryPi5_powered/total_system.py
+++ Ammeter/Tests/RaspberryPi5_powered/total_system.py
@@ -56,7 +56,7 @@ PURPLE = (100, 0, 255)
 
 # input selections
 input_names = ["OD CUP", "LE CUP", "HE CUP", "OBJ CUP", "IMG CUP", "R45 TARGET", "R30 TARGET", "L30 TARGET", "", "", "", "ERROR: INVALID INPUT"] 
-input_xlocations = [900, 500, 300, 250, 200, 800, 800, 800, 800, 800, 800, 10000]
+input_xlocations = [800, 500, 300, 250, 200, 800, 800, 800, 800, 800, 800, 10000]
 input_ylocations = [110, 100, 100, 100, 150, 800, 800, 800, 800, 800, 800, 10000]
 activeCup = 11
 
@@ -72,7 +72,7 @@ scaled_voltage = 0.0
 
 
 def draw_active_cup():
-    pygame.draw.circle(canvas, PURPLE, (input_xlocations[activeCup], input_ylocations[activeCup]), 5, 5)
+    pygame.draw.circle(canvas, PURPLE, (input_xlocations[activeCup], input_ylocations[activeCup]), 10, 10)
 
 def draw_bar(voltage):
 
