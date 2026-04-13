56a437a0bc6523459b9cb8447efc4a629195d232
unknown <mn19@uw.edu>
2026-04-12 20:52:49 -0700

    back to active cup circles

--- Ammeter/Tests/RaspberryPi5_powered/total_system.py
+++ Ammeter/Tests/RaspberryPi5_powered/total_system.py
@@ -57,8 +57,8 @@ PURPLE = (100, 0, 255)
 
 # input selections
 input_names = ["OD CUP", "LE CUP", "HE CUP", "OBJ CUP", "IMG CUP", "R45 TARGET", "R30 TARGET", "L30 TARGET", "", "", "", "ERROR: INVALID INPUT"] 
-input_xlocations = [780, 500, 300, 250, 200, 800, 800, 800, 800, 800, 800, 10000]
-input_ylocations = [107, 100, 100, 100, 150, 800, 800, 800, 800, 800, 800, 10000]
+input_xlocations = [780, 650, 450, 350, 300, 800, 800, 800, 800, 800, 800, 10000]
+input_ylocations = [107, 107, 107, 107, 150, 800, 800, 800, 800, 800, 800, 10000]
 activeCup = 11
 
 # scale selections
