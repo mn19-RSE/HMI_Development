82b5c48b419ba0db9c3285d3d7ce0a4ee167fa75
unknown <mn19@uw.edu>
2026-04-12 19:51:23 -0700

    updated webservere to be less flashy and have UDP buttons

--- Ammeter/Tests/RaspberryPi5_powered/webserver.py
+++ Ammeter/Tests/RaspberryPi5_powered/webserver.py
@@ -1,24 +1,54 @@
 from flask import Flask, send_file
+import socket
 
 app = Flask(__name__)
+
+UDP_IP = "127.0.0.1"   # sending locally to your main script
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+@app.route('/')
+def index():
+    return open("index.html").read()
+
+@app.route('/frame.jpg')
+def frame():
+    return send_file("frame.jpg", mimetype='image/jpeg')
+
+@app.route('/cmd/<command>')
+def cmd(command):
+    sock.sendto(command.encode(), (UDP_IP, UDP_PORT))
+    return "OK"
+
+app.run(host="0.0.0.0", port=5000)
 
 @app.route("/")
 def index():
     return """
     <html>
-    <body style="margin:0; background:black;">
-        <img src="/frame.jpg" id="img" width="100%">
-        <script>
-            setInterval(() => {
-                document.getElementById("img").src = "/frame.jpg?" + new Date().getTime();
-            }, 100); // ~10 FPS
-        </script>
+    <body style="margin:0; background:black; text-align:center;">
+
+    <img id="stream" src="/frame.jpg" width="800">
+
+    <br><br>
+
+    <button onclick="sendCmd('UP')">Scale Up</button>
+    <button onclick="sendCmd('DOWN')">Scale Down</button>
+
+    <script>
+    function updateImage() {
+        const img = document.getElementById("stream");
+        img.src = "/frame.jpg?t=" + new Date().getTime(); // cache buster
+    }
+
+    setInterval(updateImage, 100); // ~10 FPS
+
+    function sendCmd(cmd) {
+        fetch("/cmd/" + cmd);
+    }
+    </script>
+
     </body>
     </html>
     """
-
-@app.route("/frame.jpg")
-def frame():
-    return send_file("/tmp/frame.jpg", mimetype="image/jpeg")
-
-app.run(host="0.0.0.0", port=5000)
