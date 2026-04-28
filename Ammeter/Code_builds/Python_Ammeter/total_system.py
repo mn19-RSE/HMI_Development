415b1acf3fffbbd3465947e57faeee5153bbde91
mn19-RSE <mn19@uw.edu>
2026-04-27 18:05:58 -0700

    added repo to image filepath

--- Ammeter/Code_builds/Python_Ammeter/total_system.py
+++ Ammeter/Code_builds/Python_Ammeter/total_system.py
@@ -44,7 +44,7 @@ pygame.display.flip()
 font_small = pygame.font.SysFont(None, 50)
 font_large = pygame.font.SysFont(None, 180)
 # static image load
-vdg_logo = pygame.image.load("Ammeter/Tests/RaspberryPi5_powered/vdg.png").convert_alpha()
+vdg_logo = pygame.image.load("HMI_Development/Ammeter/Tests/RaspberryPi5_powered/vdg.png").convert_alpha()
 
 # color definitions
 WHITE = (255, 255, 255)
